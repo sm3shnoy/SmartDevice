1ace48ddec4709827165aec97dbf2fa950a29afd
sm3shnoy <eskey96@inbox.ru>
2020-10-27 00:15:47 +0400

    Добавляет подвал проекта

---
+++ build/js/main.js
@@ -0,0 +1,19 @@
+'use strict';
+
+(function () {
+  var footerToggle = document.querySelectorAll('.footer__toggle');
+  var nojs = document.querySelectorAll('.spoiler--nojs');
+
+  nojs.forEach(function (item) {
+    item.classList.remove('spoiler--nojs');
+  });
+
+  footerToggle.forEach(function (item) {
+    var spoiler = item.parentNode.nextElementSibling;
+    if (spoiler.classList.contains('spoiler')) {
+      item.addEventListener('click', function () {
+        spoiler.classList.toggle('spoiler--opened');
+      });
+    }
+  });
+})();
