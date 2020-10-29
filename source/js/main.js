581e1c29177313c848741a3037e684b7c3dafdc6
sm3shnoy <eskey96@inbox.ru>
2020-10-29 13:06:08 +0400

    добавляет плавный скрол по якорям

--- source/js/main.js
+++ source/js/main.js
@@ -17,3 +17,24 @@
     }
   });
 })();
+
+(function () {
+  const easeFunctions = {
+    easeInQuad: function (t, b, c, d) {
+      t /= d;
+      return c * t * t + b;
+    },
+    easeOutQuad: function (t, b, c, d) {
+      t /= d;
+      return -c * t* (t - 2) + b;
+    }
+  }
+
+  const moveTo = new MoveTo({
+    ease: 'easeInQuad'
+  }, easeFunctions);
+  const triggers = document.getElementsByClassName('js-scroll');
+  for (var i = 0; i < triggers.length; i++) {
+    moveTo.registerTrigger(triggers[i]);
+  }
+})();
