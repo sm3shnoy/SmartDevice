314d5ce3fe1b9631666997ca8693090d8ab4b0a2
sm3shnoy <eskey96@inbox.ru>
2020-11-11 19:42:09 +0400

    редактирует стили и скрипт для аккордеона

--- build/js/main.js
+++ build/js/main.js
@@ -152,26 +152,26 @@
   }
 })();
 
-'use strict';
+// 'use strict';
 
-(function () {
-  var footerToggle = document.querySelectorAll('.footer__toggle');
-  var spoilerNoJs = document.querySelectorAll('.spoiler--nojs');
+// (function () {
+//   var footerToggle = document.querySelectorAll('.footer__toggle');
+//   var spoilerNoJs = document.querySelectorAll('.spoiler--nojs');
 
-  for (var i = 0; i < spoilerNoJs.length; i++) {
-    spoilerNoJs[i].classList.remove('spoiler--nojs');
-  }
+//   for (var i = 0; i < spoilerNoJs.length; i++) {
+//     spoilerNoJs[i].classList.remove('spoiler--nojs');
+//   }
 
-  var spoilerClick = function (button) {
-    button.addEventListener('click', function () {
-      var spoiler = button.parentNode.nextElementSibling;
-      if (spoiler.classList.contains('spoiler')) {
-        spoiler.classList.toggle('spoiler--opened');
-      }
-    });
-  };
+//   var spoilerClick = function (button) {
+//     button.addEventListener('click', function () {
+//       var spoiler = button.parentNode.nextElementSibling;
+//       if (spoiler.classList.contains('spoiler')) {
+//         spoiler.classList.toggle('spoiler--opened');
+//       }
+//     });
+//   };
 
-  for (var j = 0; j < footerToggle.length; j++) {
-    spoilerClick(footerToggle[j]);
-  }
-})();
+//   for (var j = 0; j < footerToggle.length; j++) {
+//     spoilerClick(footerToggle[j]);
+//   }
+// })();
