585d9e6bf16750ed006cbf17799b7324018b91e2
sm3shnoy <eskey96@inbox.ru>
2020-11-16 19:28:47 +0400

    добавляет lazy loading для фоновых картинок

--- build/js/main.js
+++ build/js/main.js
@@ -1,3 +1,54 @@
+'use strict';
+
+(function () {
+  var lazyloadImages;
+
+  function lazyload() {
+    if (lazyloadThrottleTimeout) {
+      clearTimeout(lazyloadThrottleTimeout);
+    }
+
+    lazyloadThrottleTimeout = setTimeout(function () {
+      var scrollTop = window.pageYOffset;
+      lazyloadImages.forEach(function (img) {
+        if (img.offsetTop < (window.innerHeight + scrollTop)) {
+          img.src = img.dataset.src;
+          img.classList.remove('lazy');
+        }
+      });
+      if (lazyloadImages.length === 0) {
+        document.removeEventListener('scroll', lazyload);
+        window.removeEventListener('resize', lazyload);
+        window.removeEventListener('orientationChange', lazyload);
+      }
+    }, 20);
+  }
+
+  if ('IntersectionObserver' in window) {
+    lazyloadImages = document.querySelectorAll('.lazy');
+    var imageObserver = new IntersectionObserver(function (entries) {
+      entries.forEach(function (entry) {
+        if (entry.isIntersecting) {
+          var image = entry.target;
+          image.classList.remove('lazy');
+          imageObserver.unobserve(image);
+        }
+      });
+    });
+
+    lazyloadImages.forEach(function (image) {
+      imageObserver.observe(image);
+    });
+  } else {
+    var lazyloadThrottleTimeout;
+    lazyloadImages = document.querySelectorAll('.lazy');
+
+    document.addEventListener('scroll', lazyload);
+    window.addEventListener('resize', lazyload);
+    window.addEventListener('orientationChange', lazyload);
+  }
+})();
+
 
 'use strict';
 
